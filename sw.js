2e8b293a9953f33b572efd81fcfaddad3c8b44fb
grandinettirita-creator <grandinetti.rita@gmail.com>
2025-10-06 01:35:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'radio-cache-v2';
+const CACHE_NAME = 'radio-cache-v3';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -35,5 +35,6 @@ self.addEventListener('fetch', function(event) {
       })
   );
 });
+
 
 
