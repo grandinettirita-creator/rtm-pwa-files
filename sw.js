3fc87c768b31a92a856a396ce8d971a119cbc8a0
grandinettirita-creator <grandinetti.rita@gmail.com>
2025-10-06 11:33:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,28 @@
-
-const CACHE_NAME = 'radio-cache-v3';
+const CACHE_NAME = 'radio-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/192.png',
-  '/512.png',
-  '/1024.png',
-  '/sw.js'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// Installazione: memorizza tutti i file nella cache
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch: intercetta le richieste e usa la cache
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Risponde con la risorsa in cache se trovata
-        if (response) {
-          return response;
-        }
-        // Altrimenti, fa una richiesta di rete
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
-
-
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
